12f9c580bfa97dbbe5c5ccd9de4035d25063e7e2
Bill Langley <brainstormwilly@gmail.com>
2017-05-06 10:48:55 -0700

    Approved project

--- src/index.js
+++ src/index.js
@@ -1,101 +1,46 @@
 'use strict';
 var Alexa = require('alexa-sdk');
-var APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
+var APP_ID = "amzn1.ask.skill.7b180522-436d-436d-ba97-e3d5a93fe45f";  // TODO replace with your app ID (OPTIONAL).
 
 var languageStrings = {
     "en": {
         "translation": {
             "FACTS": [
-                "A year on Mercury is just 88 days long.",
-                "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-                "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
-                "On Mars, the Sun appears about half the size as it does on Earth.",
-                "Earth is the only planet not named after a god.",
-                "Jupiter has the shortest day of all the planets.",
-                "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-                "The Sun contains 99.86% of the mass in the Solar System.",
-                "The Sun is an almost perfect sphere.",
-                "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-                "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-                "The temperature inside the Sun can reach 15 million degrees Celsius.",
-                "The Moon is moving approximately 3.8 cm away from our planet every year."
+                "Cabernet Sauvignon is actually a genetic crossing of red Cabernet Franc and white Sauvignon Blanc.",
+                "The first bonded winery in California is Buena Vista Winery in Sonoma.",
+                "Zinfandel is an American synonym for a grape originally from Croatia.",
+                "Straccamyosis, the common yeast used for making bread, is also widely used in fermenting grapes.",
+                "The most predominately planted grape in France is Merlot.",
+                "Over 80% of all vines in Europe and America are planted on American rootstock to prevent a root louse infestation.",
+                "The grape species known as Vitis Vinifera, which is most commonly used for making wine, was first discovered in the Caucasus Mountains in the country of Georgia.",
+                "most red wine gets its color from grape skins in a process known as maceration.",
+                "A grape vine, in the right conditions, can grow roots more than 25 feet deep.",
+                "Each grape bunch, on average, will produce one glass of wine.",
+                "Each grape vine, on average, will produce one-to-two bottles of wine.",
+                "The first American Viticultural Area, or A.V.A., was established in the state of Missouri.",
+                "California produces over 80% of all wine produced in the U.S.A.",
+                "Cava is made using the same methods as in Champagne, but with different grapes: Macabayo, Parreeyada, and Cherello",
+                "A tradional oak barrel, or barreeck, holds approximately 60 gallons or 25 cases of wine.",
+                "A bottle of sparkling wine can contain 5 atmospheres of pressure, or the equivalent of a truck tire.",
+                "In Australia, Shiraz is the preferred synonym for Syrah and named after the ancient Persian city from where it was first discovered.",
+                "The Salta Province in Argentina is both the highest wine region in elevation and closest to the equator.",
+                "Ice wine grapes are both harvested and crushed while frozen.",
+                "Cote du Rhone wines are, by law, required to be fermented in concrete vessels.",
+                "All five Bordeaux 'First growth' producers were classified in 1855, except one. Chateau Mouton-Rothschild was controversally added in 1972.",
+                "All vitis Vinifera varietals genetically originate from an ancient grape called Casanova... which makes terrible wine."
             ],
-            "SKILL_NAME" : "Space Facts",
+            "SKILL_NAME" : "Wine Facts",
             "GET_FACT_MESSAGE" : "Here's your fact: ",
-            "HELP_MESSAGE" : "You can say tell me a space fact, or, you can say exit... What can I help you with?",
+            "HELP_MESSAGE" : "You can say tell me a wine fact, or, you can say exit... What can I help you with?",
             "HELP_REPROMPT" : "What can I help you with?",
             "STOP_MESSAGE" : "Goodbye!"
-        }
-    },
-    "en-US": {
-        "translation": {
-            "FACTS": [
-                "A year on Mercury is just 88 days long.",
-                "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-                "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
-                "On Mars, the Sun appears about half the size as it does on Earth.",
-                "Earth is the only planet not named after a god.",
-                "Jupiter has the shortest day of all the planets.",
-                "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-                "The Sun contains 99.86% of the mass in the Solar System.",
-                "The Sun is an almost perfect sphere.",
-                "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-                "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-                "The temperature inside the Sun can reach 15 million degrees Celsius.",
-                "The Moon is moving approximately 3.8 cm away from our planet every year."
-            ],
-            "SKILL_NAME" : "American Space Facts"
-        }
-    },
-    "en-GB": {
-        "translation": {
-            "FACTS": [
-                "A year on Mercury is just 88 days long.",
-                "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-                "Venus rotates anti-clockwise, possibly because of a collision in the past with an asteroid.",
-                "On Mars, the Sun appears about half the size as it does on Earth.",
-                "Earth is the only planet not named after a god.",
-                "Jupiter has the shortest day of all the planets.",
-                "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-                "The Sun contains 99.86% of the mass in the Solar System.",
-                "The Sun is an almost perfect sphere.",
-                "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-                "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-                "The temperature inside the Sun can reach 15 million degrees Celsius.",
-                "The Moon is moving approximately 3.8 cm away from our planet every year."
-            ],
-            "SKILL_NAME" : "British Space Facts"
-        }
-    },
-    "de": {
-        "translation": {
-            "FACTS": [
-                "Ein Jahr dauert auf dem Merkur nur 88 Tage.",
-                "Die Venus ist zwar weiter von der Sonne entfernt, hat aber höhere Temperaturen als Merkur.",
-                "Venus dreht sich entgegen dem Uhrzeigersinn, möglicherweise aufgrund eines früheren Zusammenstoßes mit einem Asteroiden.",
-                "Auf dem Mars erscheint die Sonne nur halb so groß wie auf der Erde.",
-                "Die Erde ist der einzige Planet, der nicht nach einem Gott benannt ist.",
-                "Jupiter hat den kürzesten Tag aller Planeten.",
-                "Die Milchstraßengalaxis wird in etwa 5 Milliarden Jahren mit der Andromeda-Galaxis zusammenstoßen.",
-                "Die Sonne macht rund 99,86 % der Masse im Sonnensystem aus.",
-                "Die Sonne ist eine fast perfekte Kugel.",
-                "Eine Sonnenfinsternis kann alle ein bis zwei Jahre eintreten. Sie ist daher ein seltenes Ereignis.",
-                "Der Saturn strahlt zweieinhalb mal mehr Energie in den Weltraum aus als er von der Sonne erhält.",
-                "Die Temperatur in der Sonne kann 15 Millionen Grad Celsius erreichen.",
-                "Der Mond entfernt sich von unserem Planeten etwa 3,8 cm pro Jahr."
-            ],
-            "SKILL_NAME" : "Weltraumwissen auf Deutsch",
-            "GET_FACT_MESSAGE" : "Hier sind deine Fakten: ",
-            "HELP_MESSAGE" : "Du kannst sagen, „Nenne mir einen Fakt über den Weltraum“, oder du kannst „Beenden“ sagen... Wie kann ich dir helfen?",
-            "HELP_REPROMPT" : "Wie kann ich dir helfen?",
-            "STOP_MESSAGE" : "Auf Wiedersehen!"
         }
     }
 };
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
-    alexa.APP_ID = APP_ID;
+    alexa.APP_ID = "amzn1.ask.skill.7b180522-436d-436d-ba97-e3d5a93fe45f";
     // To enable string internationalization (i18n) features, set a resources object.
     alexa.resources = languageStrings;
     alexa.registerHandlers(handlers);
